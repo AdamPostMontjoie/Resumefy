9fd590920e2a9cf35ba75ae6fd6ed15d2edfef20
YunkunWang <yunkun121818@gmail.com>
2025-10-06 02:14:40 -0400

    basic login/registration setup

--- frontend/src/routes/Home/App.jsx
+++ frontend/src/routes/Home/App.jsx
@@ -1,13 +1,18 @@
-import React from "react"
-
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+
+  const goToLogin = () => {
+    navigate("/login");
+  };
 
   return (
-    <>
-      <h1>Home page</h1>
-    </>
-  )
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Home Page</h1>
+      <button onClick={goToLogin}>Login Page</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
