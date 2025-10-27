9aa9dcb154f006be921703681bcd22bd15b9bd9c
KeeganGallagher5959 <keegangalla1@gmail.com>
2025-10-27 16:44:26 -0400

    installed axion, puppeteer, and set credential to default

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,10 +2,8 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./resumefy-8e8fa-596b083f089f.json');
-
 initializeApp({
-  credential: cert(serviceAccount)
+  credential: applicationDefault()
 });
 
 const db = getFirestore();
