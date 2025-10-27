18857292c8b52b743e631f5639d1d86d8b2ba6eb
KeeganGallagher5959 <keegangalla1@gmail.com>
2025-10-27 19:24:11 -0400

    switch firebase.js to how it was before

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,9 +1,10 @@
 
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const serviceAccount = require('./resumefy-8e8fa-596b083f089f.json');
 
 initializeApp({
-  credential: applicationDefault()
+  credential: cert(serviceAccount)
 });
 
 const db = getFirestore();
