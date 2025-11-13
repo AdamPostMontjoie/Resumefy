a240af46031a4228e4f52185629560b48478e5c9
cerenoguz1 <coguz@umass.edu>
2025-11-12 20:19:47 -0500

    Finalize server.js with dotenv env setup

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
+require('dotenv').config();
 const express = require('express');
-const admin = require('firebase-admin');
 const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const admin = require('firebase-admin');
 
 const app = express();
 app.use(express.json());
 
-// initializizing Firebase
-const serviceAccount = require('./firebase.json');
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
 const db = admin.firestore();
+
 
 // for latex generation
 function generateLatex(data) {
